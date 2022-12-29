4e8f20fd1bd91b06c91cb6aee16e53d02794b935
pradyutnair <p.nair@student.tue.nl>
2022-12-29 13:24:50 +0100

    Final

---
+++ pages/api/generate.js
@@ -0,0 +1,31 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = "";
+
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
+  const model_prompt = `Given the following database schema and their properties: ${req.body.databaseSchema}, Write an SQL query to ${req.body.userInput}. You may use joins and sub-queries and except clauses if necessary: SELECT`;
+  const baseCompletion = await openai.createCompletion({
+    model: 'code-davinci-002',
+    prompt: model_prompt,
+    temperature: 0,
+    max_tokens: 150,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stop: ["#", ";"],
+  });
+
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,120 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { useState } from 'react';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import arrow from '../assets/arrow.webp';
+import trip from '../assets/trip.gif';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+  const [databaseSchema, setDatabaseSchema] = useState('');
+  const [testOutput, setTestOutput] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false)
+  let result = 'SELECT ';
+  const callGenerateEndpoint = async () => {
+    if (userInput === "") {
+      // display error message
+      alert("Please enter a query before generating.");
+    } else {
+      setIsGenerating(true);
+
+      console.log("Calling OpenAI...")
+      const response = await fetch('/api/generate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({userInput, databaseSchema}),
+      });
+
+      const data = await response.json();
+      const {output} = data;
+      console.log("OpenAI replied...", output.text)
+
+      setApiOutput(`${output.text}`);
+      setIsGenerating(false);
+    }
+  }
+  const onUserChangedText = (event) => {
+    //console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+  const onDatabaseChangedText = (event) => {
+    //console.log(event.target.value);
+    setDatabaseSchema(event.target.value);
+  };
+  const onTestOutputText = (event) => {
+    setTestOutput(event.target.value);
+  }
+
+  function copyToClipboard(elementId) {
+  // select the textarea
+  var textarea = document.getElementById(elementId);
+  textarea.select();
+
+  // copy the text to the clipboard
+  document.execCommand("copy");
+}
+
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
+
       <div className="container">
+        <div className="prompt-container logo-container">
+          <div className="logo"></div>
+        </div>
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>English to SQL</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Easily convert plain english to SQL queries!</h2>
           </div>
+        </div>
+        <div className="query-container">
+          <textarea placeholder="state your query"
+                    className="prompt-box query-box"
+                    value={userInput}
+                    onChange={onUserChangedText}
+
+          />
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={callGenerateEndpoint}>
+              <div className="generate">
+                {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className="prompt-container">
+          <textarea placeholder="enter your schema"
+                    className="prompt-box"
+                    value={databaseSchema}
+                    onChange={onDatabaseChangedText}
+          />
+          {apiOutput && (
+          <textarea placeholder="output..."
+                    className="prompt-box"
+                    value={result + apiOutput.replace("<code>", "")}
+                    onChange={onTestOutputText}
+                    readOnly={true}
+          />
+          )}
+
+           {/*New code I added here */}
+            
         </div>
       </div>
       <div className="badge-container grow">
         <a
-          href="https://buildspace.so/builds/ai-writer"
+          href="https://github.com/pradyutnair"
           target="_blank"
           rel="noreferrer"
         >
           <div className="badge">
             <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <p>GitHub</p>
           </div>
         </a>
       </div>
