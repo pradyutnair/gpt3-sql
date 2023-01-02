946c4b3a43c88f63defaa64ba6dde494055e5a09
pradyutnair <83001928+pradyutnair@users.noreply.github.com>
2023-01-02 19:02:20 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ const Home = () => {
       setIsGenerating(true);
 
       console.log("Calling OpenAI...")
-      const response = await fetch('./api/generate', {
+      const response = await fetch('../api/generate', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
